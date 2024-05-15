04b00ecc9e6c6cae26d5aec8159d4e87b5f6cd33
sharmvan <vandanas020@gmail.com>
2024-05-15 07:46:57 +0530

    May 15, 2024

--- Mar 2024/01_March_2024/Lab_144.py
+++ Mar 2024/01_March_2024/Lab_144.py
@@ -1,5 +1,4 @@
 # What if file is present somewhere else?
-path = "C:\Users\Sharmvan\PycharmProjects\PythonLearning2x\Feb 2024\feb.txt"
+path = r"C:\Users\Sharmvan\PycharmProjects\PythonLearning2x\Feb 2024\feb.txt"
 with open(path) as file:
     print(file.read())
-
